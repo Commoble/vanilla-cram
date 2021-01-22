9f17dc58096aafb1f813624172e976f618430e31
Commoble <josephbettendorff@yahoo.com>
2021-01-22 14:11:32 -0600

    Setup Workspace

---
+++ src/main/java/commoble/vanillacram/VanillaCram.java
@@ -0,0 +1,15 @@
+package commoble.vanillacram;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(VanillaCram.MODID)
+public class VanillaCram
+{
+	public static final String MODID = "vanilla-cram";
+	
+	// invoked by forge due to @Mod
+	public VanillaCram()
+	{
+		
+	}
+}
