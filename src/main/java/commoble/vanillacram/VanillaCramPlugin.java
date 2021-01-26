0374f287b36cd6e94b45cae8e5598f5530e68dcb
Commoble <josephbettendorff@yahoo.com>
2021-01-26 16:10:33 -0600

    Automatically register cram behaviours for all pressure plates, mark vanilla pressure plates crammable

--- src/main/java/commoble/vanillacram/VanillaCramPlugin.java
+++ src/main/java/commoble/vanillacram/VanillaCramPlugin.java
@@ -3,20 +3,30 @@ package commoble.vanillacram;
 import commoble.cram.api.AutoCramPlugin;
 import commoble.cram.api.CramPlugin;
 import commoble.cram.api.functions.CramRegistrator;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.AbstractPressurePlateBlock;
+import net.minecraft.block.Block;
+import net.minecraftforge.registries.ForgeRegistries;
 
-@AutoCramPlugin
+// negative priority makes it easier for other plugins to override behaviours for their own pressure plates, etc
+@AutoCramPlugin(priority=-1)
 public class VanillaCramPlugin implements CramPlugin
 {
 
 	@Override
 	public void register(CramRegistrator registry)
 	{
-		registry.getCramEntry(Blocks.STONE_PRESSURE_PLATE)
-			.setEntityCollisionBehavior(PressurePlateBehaviors::onEntityCollision)
-			.setScheduledTickBehavior(PressurePlateBehaviors::onScheduledTick);
+		ForgeRegistries.BLOCKS.forEach(block -> doBlockRegistry(registry, block));
 		
 		VanillaCram.LOGGER.info("Loaded Cram Plugin for vanilla objects");
 	}
 
+	static void doBlockRegistry(CramRegistrator registry, Block block)
+	{
+		if (block instanceof AbstractPressurePlateBlock)
+		{
+			registry.getCramEntry(block)
+				.setEntityCollisionBehavior(PressurePlateBehaviors::onEntityCollision)
+				.setScheduledTickBehavior(PressurePlateBehaviors::onScheduledTick);
+		}
+	}
 }
